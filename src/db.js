a2c5de0fc32fc8087d4d1fd029f4123230562b3a
JeroJamanoy <jerojamanoy2810@gmail.com>
2025-04-12 14:45:59 -0500

    Sirve todo

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async() => {
     try {
         // Usar MongoDB Atlas
-        const uri = process.env.MONGODB_URI ||  "mongodb+srv://root:root@cluster0.vd0vupk.mongodb.net/certicampo?retryWrites=true&w=majority&appName=Cluster0";
+        const uri = process.env.MONGODB_URI ||  "mongodb+srv://root:root@cluster0.vd0vupk.mongodb.net/Certicampo?retryWrites=true&w=majority&appName=Cluster0";
 
         await mongoose.connect(uri, {
             serverSelectionTimeoutMS: 5000, // Timeout después de 5 segundos
@@ -35,22 +35,7 @@ async function createTestUsers() {
     ];
     
     // Reemplazar métodos de User.findOne y User.findById
-    mongoose.model = function(name) {
-        if (name === 'User') {
-            return {
-                findOne: async function(query) {
-                    if (query.userid) {
-                        return global.users.find(user => user.userid === query.userid) || null;
-                    }
-                    return null;
-                },
-                findById: async function(id) {
-                    return global.users.find(user => user._id === id) || null;
-                }
-            };
-        }
-        return {};
-    };
+
     
     console.log("Base de datos en memoria creada con usuario de prueba");
     console.log("Puedes iniciar sesión con: 1054859914 y contraseña: 1054859914");
